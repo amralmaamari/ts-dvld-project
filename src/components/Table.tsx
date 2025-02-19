8cca828a7e2d59ad71b029e14f3c10a6ec2c88ae
fd <amr.almaamari@gmail.com>
2025-02-20 00:40:18 +0530

    Complete table component implementation

---
+++ src/components/Table.tsx
@@ -0,0 +1,43 @@
+import React, { JSX, ReactNode } from "react";
+import "./Table.css";
+
+interface ICoulmn{
+  header:string,
+  accessor:string,
+  className?:string,
+
+}
+interface ITableProps<T>{
+  columns:ICoulmn[];
+  data:T[];
+  renderRow:(row:T)=>ReactNode;
+
+
+}
+export default function Table<T>({ columns, renderRow, data }:ITableProps<T>):JSX.Element {
+  return (
+    <div className="h-[400px] border-collapse text-left table-container  overflow-auto rounded-lg">
+      <table className="w-full bg-slate-200 ">
+        <thead className="sticky top-0 bg-primary text-white py-3">
+          <tr className="py-3 px-6">
+            {columns.map((column) => (
+              <th
+                key={column.accessor}
+                className={`py-3 px-6 text-left text-lg font-semibold ${
+                  column.className || ""
+                }`}
+              >
+                {column.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-300">
+          {data.map((row, index) => (
+              <React.Fragment key={index}>{renderRow(row)}</React.Fragment>
+            ))}      
+        </tbody>
+      </table>
+    </div>
+  );
+}
